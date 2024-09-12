f0a090a50eb978528d3d4a90bedd1c8bad07dad9
DESKTOP-3FFMDEV\admin <2229608151@qq.com>
2024-09-12 11:25:18 +0800

    提交

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
+import re
 import logging
 import stripe
-from telegram import Update, LabeledPrice, ReplyKeyboardMarkup,ShippingOption
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext, PreCheckoutQueryHandler
+from telegram import Update, LabeledPrice, ReplyKeyboardMarkup, ShippingOption, InlineQueryResultArticle, InputTextMessageContent, InlineQueryResultPhoto
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext, PreCheckoutQueryHandler, InlineQueryHandler
 
 
 # 配置 Stripe 密钥
@@ -13,8 +14,10 @@ TELEGRAM_TOKEN = '7487357401:AAHRuJa0dmMj4Fk4c1wTg2wgu01q5xShV84'
 # 配置支付提供者令牌
 PAYMENT_PROVIDER_TOKEN = '284685063:TEST:NzBhNjk4ZjIyMjBl'
 
+
 async def start(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text('欢迎使用机器人')
+
 
 async def request_invoice(update: Update, context: CallbackContext) -> None:
     try:
@@ -38,13 +41,100 @@ async def request_invoice(update: Update, context: CallbackContext) -> None:
     except Exception as e:
         await update.message.reply_text('创建发票时出现问题，请稍后再试。')
 
+# 预付款查询监听（支付前检查）
+
+
 async def pre_checkout_query(update: Update, context: CallbackContext) -> None:
     query = update.pre_checkout_query
     await context.bot.answer_pre_checkout_query(query.id, ok=True)
 
+# 支付成功监听
+
+
 async def successful_payment(update: Update, context: CallbackContext) -> None:
     payment = update.message.successful_payment
     await update.message.reply_text('感谢您的付款！')
+
+
+async def inline_query(update: Update, context: CallbackContext) -> None:
+    query = update.inline_query.query
+    results = []
+
+    if query == '':
+        print(1)
+        results.append(
+            InlineQueryResultArticle(
+                id='1',
+                title='请输入支付信息',
+                input_message_content=InputTextMessageContent('请提供支付相关的信息。'),
+                thumbnail_url='https://space.yygogogo.com//storage/product/20240911/9a/bac2338f5fec5260c58a703ac5766126baf02d.png',  # 可选
+                description='用于支付的内联查询'
+
+            )
+        )
+    elif 'payment' in query:
+        print(2)
+        results.append(
+            InlineQueryResultArticle(
+                id='2',
+                title='支付处理',
+                input_message_content=InputTextMessageContent(
+                    '处理支付请求: {}'.format(query)),
+                thumbnail_url='https://space.yygogogo.com//storage/product/20240911/9a/bac2338f5fec5260c58a703ac5766126baf02d.png',  # 可选
+                description='确认您的支付信息'
+            )
+        )
+    else:
+        print(3)
+        results.append(
+            # 返回文章结果
+            # InlineQueryResultArticle(
+            #     id='3',
+            #     title='帮助',
+            #     # input_message_content=InputTextMessageContent('请在查询中包含“payment”以处理支付。'),
+            #     input_message_content=InputTextMessageContent('生成订单成功'),
+            #     thumbnail_url='https://space.yygogogo.com//storage/product/20240911/9a/bac2338f5fec5260c58a703ac5766126baf02d.png',  # 可选
+            #     description='帮助信息'
+            # ),
+            InlineQueryResultArticle(
+            id="3",  # 唯一ID
+            title="点击生成发票",
+            input_message_content=InputTextMessageContent('请点击生成支付发票。')
+            )
+            # 返回图片结果
+            # InlineQueryResultPhoto(
+            #     id='1',
+            #     photo_url='https://space.yygogogo.com//storage/product/20240911/9a/bac2338f5fec5260c58a703ac5766126baf02d.png',  # 你的图片 URL
+            #     thumbnail_url='https://space.yygogogo.com//storage/product/20240911/9a/bac2338f5fec5260c58a703ac5766126baf02d.png',  # 缩略图 URL
+            #     title='示例图片',
+            #     description='这是一个图片的例子',
+            #     caption='这是图片的说明'
+            # )
+        )
+    await context.bot.answer_inline_query(update.inline_query.id, results)
+
+
+async def custom_message_handler(update: Update, context: CallbackContext) -> None:
+    print("hao hao")
+    message_text = update.message.text
+    # 定义正则表达式模式
+    pattern = '生成订单成功'
+    # 使用正则表达式匹配消息内容
+    if re.search(pattern, message_text):
+        await request_invoice(update, context)
+
+
+# 处理消息并应用正则表达式过滤器
+async def handle_message(update: Update, context: CallbackContext) -> None:
+    message_text = update.message.text
+    print(1111111)
+    # 检查消息是否符合正则表达式
+    if regex_filter(message_text, r'生成支付发票'):
+        await request_invoice(update, context)
+
+# 自定义的正则表达式过滤器
+def regex_filter(pattern):
+    return lambda message: re.search(pattern, message.text) is not None
 
 def main() -> None:
     # 创建 Application 对象
@@ -54,11 +144,20 @@ def main() -> None:
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CommandHandler('pay', request_invoice))
     # application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, request_invoice))
+    # application.add_handler(MessageHandler(filters.TEXT, custom_message_handler))
+
+    # 处理所有文本消息，并在消息处理函数中应用正则表达式过滤器
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
     application.add_handler(PreCheckoutQueryHandler(pre_checkout_query))
-    application.add_handler(MessageHandler(filters.SUCCESSFUL_PAYMENT, successful_payment))
+    application.add_handler(MessageHandler(
+        filters.SUCCESSFUL_PAYMENT, successful_payment))
+
+    application.add_handler(InlineQueryHandler(inline_query))
 
     # 启动机器人
     application.run_polling()
+
 
 if __name__ == '__main__':
     main()
